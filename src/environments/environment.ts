003d8884e4d91c8f00ff32c19e7cc07cf8113e06
Ayesha Javaid <ayeshajavaid5005@gmail.com>
2025-11-14 13:04:45 +0500

    Masle haal ho gaye yayyy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,13 @@
 export const environment = {
   production: false,
   google: {
-    clientId: 'YOUR_GOOGLE_CLIENT_ID',
-    apiKey: 'YOUR_GOOGLE_API_KEY', // optional but recommended
+    clientId:
+      '814378952571-kmsheoit1993iah6u3ut0p810vqa1nf8.apps.googleusercontent.com',
+    apiKey: 'AIzaSyCDXvilf4BgmTr7glhDLbzH8pL8IinkxzE', // optional but recommended
+    discoveryDocs: [
+      'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest',
+    ],
+    scope: 'https://www.googleapis.com/auth/calendar.events',
   },
   emailJS: {
     serviceID: 'service_e3mag1o',
